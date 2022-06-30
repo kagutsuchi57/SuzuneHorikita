71b846c1b2f5b72dc96e25a9ec127b6cfc593600
nitanuj <103054544+nitanuj@users.noreply.github.com>
2022-06-30 23:17:12 +0530

    Update __init__.py

--- SuzuneHorikita/modules/sql/__init__.py
+++ SuzuneHorikita/modules/sql/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine("postgres://ycuonyceejuevz:c107e86c014c8ea740e3dc51247e308126625a546c5add5ceb1e2389d05eb828@ec2-3-224-8-189.compute-1.amazonaws.com:5432/d83c311ir863sb", client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=True))
